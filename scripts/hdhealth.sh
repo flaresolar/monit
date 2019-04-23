5260d2357b2d012bb79716eebb5a43ed96c56677
Eli Dagan <s***@***.com>
2019-04-24 00:00:01 +0300

    monit updated

--- scripts/hdhealth.sh
+++ scripts/hdhealth.sh
@@ -2,13 +2,22 @@
 TP=-1
 STATUS=`/usr/sbin/smartctl -H /dev/sd${1} | grep overall-health | awk 'match($0,"result:"){print substr($0,RSTART+8,6)}'`
 
+/usr/sbin/smartctl -H /dev/sd${1} | grep overall-health
+echo 
+/usr/sbin/smartctl -l selftest /dev/sd${1}| sed '1,5d;$d'
+echo 
+/usr/sbin/smartctl -i /dev/sd${1} | sed '1,3d;$d'
+#/usr/sbin/smartctl -a /dev/sd${1} | grep Serial
+
+
 if [ "$?" = "0" ]; then 
   if [ "$STATUS" = "PASSED" ] 
   then
      # 1 implies PASSED
      TP=0
-     /usr/sbin/smartctl -H /dev/sd${1} | grep overall-health
-     /usr/sbin/smartctl -l selftest /dev/sd${1} || TP=1
+     #/usr/sbin/smartctl -H /dev/sd${1} | grep overall-health
+     /usr/sbin/smartctl -q silent  -l selftest /dev/sd${1} || TP=1
+     echo 
      /usr/sbin/smartctl -a /dev/sd${1} | grep "Self-test execution status:      (   0)" || (TP=1 && /usr/sbin/smartctl -a /dev/sd${1})
   else 
      # 2 implies FAILED
